a2b8fb838d853a7570014b0e34f600023bb705be
MSKose <saitmustafa1998@gmail.com>
2023-05-04 00:05:13 +0200

    lecture8

---
+++ src/javalecture8/MainClass.java
@@ -0,0 +1,18 @@
+package javalecture8;
+
+public class MainClass {
+	public static void main(String[] args) {
+		int num1 = 20;
+		int num2 = 40;
+		
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		num1++;
+		num2--;
+		 
+		System.out.println(num1);
+		System.out.println(num2);
+	}
+}
---
+++ src/javalecture8/Practice01.java
@@ -0,0 +1,24 @@
+package javalecture8;
+
+public class Practice01 {
+
+	public static void main(String[] args) {
+		
+		int s1 = 1;
+		int s2 = 1;
+		
+		System.out.println(s1 + "x" + s2 + "=" + s1 * s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1 * s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1 * s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1 * s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1 * s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1 * s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1 * s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1 * s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1 * s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1 * s2++);
+		
+		
+	}
+
+}
