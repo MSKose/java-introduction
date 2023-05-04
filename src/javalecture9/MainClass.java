d9eb07200157dd8060aa4a7b27ed6ee84e019121
MSKose <saitmustafa1998@gmail.com>
2023-05-04 23:07:52 +0200

    lecture9

---
+++ src/javalecture9/MainClass.java
@@ -0,0 +1,26 @@
+package javalecture9;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		int num = 20;
+		
+		num *= 5;
+		System.out.println(num);
+		
+		num += 5;
+		System.out.println(num);
+		
+		num -= 5;
+		System.out.println(num);
+		
+		num /= 20;
+		System.out.println(num);
+		
+		num %= 5;
+		System.out.println(num);
+
+	}
+
+}
---
+++ src/javalecture9/Practice01.java
@@ -0,0 +1,12 @@
+package javalecture9;
+
+public class Practice01 {
+	public static void main(String[] args) {
+		
+		 int num1 = 25;
+		 int num2 = 5;
+		 num1 *= num2;
+		 System.out.println(num1);
+		 
+	}
+}
